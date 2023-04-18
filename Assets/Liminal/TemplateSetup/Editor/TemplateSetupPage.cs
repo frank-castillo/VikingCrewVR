6cd206f19b6bc420465df4e0f9a6c1223d3104d8
Francisco Castillo <78894868+frank-castillo@users.noreply.github.com>
2023-04-17 19:46:08 -0700

    [Main Branch] Folder Setup

--- Assets/Liminal/TemplateSetup/Editor/TemplateSetupPage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Liminal.Editor.TemplateSetup
-{
-    public abstract class TemplateSetupPage
-    {
-        public abstract bool CanProceed { get; }
-        public abstract string Name { get; }
-        public abstract void DrawPage();
-    }
-}
