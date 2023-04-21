6dcceacb388fc516684f563fa8bd7dcd4aee371f
Gary <56449687+JustMeGary@users.noreply.github.com>
2023-04-21 14:56:21 -0700

    Added Assets Placement
    - Added Ship, Drum & Mjolnir (Hammer)
    - Added Ship and Drum Script
    - Added Drum Animation
    - Added Probuilder
    - Added Skybox

---
+++ Assets/GaryDumbFolder/VikingShipController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VikingShipController : MonoBehaviour
+{
+    [Header("Ship's Buoyancy Controllers")]
+    [SerializeField] private bool enableBuoyancy = false;
+
+    [Space]
+
+    [SerializeField] [Range(0, 10)] private float height = 0.1F;
+    [SerializeField] [Range(0, 10)] private float speedForY = 1F;
+
+    [Space]
+
+    [SerializeField] [Range(0, 10)] private float width = 0.05F;
+    [SerializeField] [Range(0, 10)] private float speedForX = 2F;
+    private Vector3 shipPosition;
+
+    private void Start()
+    {
+        shipPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        BuoyancyEffect();
+    }
+
+    private void BuoyancyEffect()
+    {
+        if (!enableBuoyancy) return;
+
+        float newY = shipPosition.y + height * Mathf.Sin(Time.time * speedForY);
+        float newX = shipPosition.x + width * Mathf.Sin(Time.time * speedForX);
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+
+}
