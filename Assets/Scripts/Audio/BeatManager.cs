3d9155c7f04b3fb65fba8cb462d285bb92c6efa9
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2023-04-24 19:29:19 -0700

    [Beat Manager Branch] Setup

--- Assets/Scripts/Audio/BeatManager.cs
+++ Assets/Scripts/Audio/BeatManager.cs
@@ -2,7 +2,23 @@
 
 public class BeatManager : MonoBehaviour
 {
+    [Header("Beat Timers")]
+    [SerializeField] private float _beatDelay = 0.75f;
+    [SerializeField] private float _hitWindow = 0.25f;
+
+    [Header("References")]
     [SerializeField] private FeedbackManager _feedbackManager = null;
+
+    private float _timer = 0.0f;
+    private bool _isOnBeat = false;
+    private bool _isPlaying = false;
+
+    public void StopBeat() { _isPlaying = false; }
+    public void StartBeat()
+    {
+        _isPlaying = true;
+        _timer = _beatDelay;
+    }
 
     public BeatManager Initialize()
     {
@@ -11,5 +27,47 @@ public class BeatManager : MonoBehaviour
         _feedbackManager.Initialize();
 
         return this;
+    }
+
+    private void Update()
+    {
+        if (_isPlaying == false)
+        {
+            return;
+        }
+
+        _timer -= Time.deltaTime;
+
+        if (_timer < 0)
+        {
+            OnBeatSwap();
+        }
+    }
+
+    private void OnBeatSwap()
+    {
+        _isOnBeat = !_isOnBeat;
+        if (_isOnBeat)
+        {
+            Debug.Log($"Start Beat: Hit now!");
+            _timer = _hitWindow;
+        }
+        else
+        {
+            Debug.Log($"End Beat: Stop hitting drums");
+            _timer = _beatDelay;
+        }
+    }
+
+    public void Hit()
+    {
+        if (_isOnBeat)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
