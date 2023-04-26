caf1eac2d430c82d8606201bf338e0f561855090
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2023-04-26 16:35:10 -0700

    [Feedback Manager Branch] Redid Feedback Manager Implementation

--- Assets/Scripts/Core/Feedback/FeedbackManager.cs
+++ Assets/Scripts/Core/Feedback/FeedbackManager.cs
@@ -1,11 +1,31 @@
 ﻿using UnityEngine;
+using System;
 
 public class FeedbackManager : MonoBehaviour
 {
+    public Action onBeat = null;
+    public Action<int> onBeatHit = null;
+    public Action offBeatMiss = null;
+
     public FeedbackManager Initialize()
     {
         Debug.Log($"<color=Lime> {this.GetType()} starting setup. </color>");
 
         return this;
+    }
+
+    public void StandardBeatFeedback()
+    {
+        onBeat?.Invoke();
+    }
+
+    public void OnBeatFeedback(int streak)
+    {
+        onBeatHit?.Invoke(streak);
+    }
+
+    public void OffBeatFeedback()
+    {
+        offBeatMiss?.Invoke();
     }
 }
