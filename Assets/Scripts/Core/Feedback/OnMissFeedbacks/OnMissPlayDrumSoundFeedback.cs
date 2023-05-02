f32a855d2ea98aee77b60e68dd1d79180ce5a2de
Francisco Castillo <78894868+frank-castillo@users.noreply.github.com>
2023-05-01 19:44:03 -0700

    [Audio Feedback Refactor] Refactor Audio Feedback
    - Add audio feedback scripts for the drum

---
+++ Assets/Scripts/Core/Feedback/OnMissFeedbacks/OnMissPlayDrumSoundFeedback.cs
@@ -0,0 +1,22 @@
+﻿public class OnMissPlayDrumSoundFeedback : Feedback
+{
+    private AudioManager _audioManager = null;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _audioManager = ServiceLocator.Get<AudioManager>();
+    }
+
+    public override void Play()
+    {
+        base.Play();
+        _audioManager.PlayOffBeatDrumSound();
+    }
+
+    public override void Stop()
+    {
+        base.Stop();
+    }
+}
