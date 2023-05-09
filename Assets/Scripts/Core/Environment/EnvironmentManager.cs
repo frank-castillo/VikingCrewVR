97bfc45d71330c801b9df56227a2492036b81548
Francisco Castillo <78894868+frank-castillo@users.noreply.github.com>
2023-05-08 19:18:41 -0700

    [week-5-ship-movement] Add Region Treadmill
    - Add Treadmill script
    - Create region prefabs
    - Add Environment manager
    - Fix typos on feedbacks

---
+++ Assets/Scripts/Core/Environment/EnvironmentManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    [SerializeField] private RegionTreadmill _environmentTreadmill = null;
+
+    public void Initialize()
+    {
+        Debug.Log($"<color=Lime> {this.GetType()} starting setup. </color>");
+
+        if (_environmentTreadmill != null)
+        {
+            _environmentTreadmill.Initialize();
+        }
+    }
+
+    public void StartEnvironment()
+    {
+        _environmentTreadmill.EnableMovement(true);
+    }
+}
