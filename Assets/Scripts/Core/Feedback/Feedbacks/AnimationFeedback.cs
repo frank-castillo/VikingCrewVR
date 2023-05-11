4a69c7968ccb2276329ea765ac14878abfb81959
Francisco Castillo <78894868+frank-castillo@users.noreply.github.com>
2023-05-10 17:39:33 -0700

    [week-5-additional-feedbacks] Add Additional feedbacks
    - Added Animation Feedback
    - Added Splash feedback
    - Added Viking Chant feedback
    - Added Ticking feedback event handlers to environment and crew
    - Update Drum and Ship prefab

---
+++ Assets/Scripts/Core/Feedback/Feedbacks/AnimationFeedback.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class AnimationFeedback : Feedback
+{
+    [SerializeField] private Animator _animator = null;
+    [SerializeField] private string _triggerName = string.Empty;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    public override void Play()
+    {
+        base.Play();
+
+        _animator.SetTrigger(_triggerName);
+    }
+
+    public override void Stop()
+    {
+        base.Stop();
+    }
+}
--- Assets/Scripts/Core/Feedback/UnityEventFeedback.cs
+++ Assets/Scripts/Core/Feedback/UnityEventFeedback.cs
@@ -2,7 +2,6 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-
 public class UnityEventFeedback : Feedback
 {
     [SerializeField] private UnityEvent _onPlay = null;
