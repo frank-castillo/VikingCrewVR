d7560ad55f0441f968cfcf6fd6ea29084e8e6faf
Francisco Castillo <78894868+frank-castillo@users.noreply.github.com>
2023-05-17 15:20:01 -0700

    []

--- Assets/Scripts/Systems/ShowHideControllers.cs
+++ Assets/Scripts/Systems/ShowHideControllers.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Liminal.SDK.VR.Avatars;
 
 public class ShowHideControllers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializedField] public VRAvatarController PrimaryController;
+    public VRAvatarController SecondaryController;
+
+    public bool ShowPrimary;
+    public bool ShowSecondary;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PrimaryController != null && PrimaryController.gameObject.activeSelf != ShowPrimary)
+        {
+            PrimaryController.gameObject.SetActive(ShowPrimary);
+        }
+
+        if (SecondaryController != null && SecondaryController.gameObject.activeSelf != ShowSecondary)
+        {
+            SecondaryController.gameObject.SetActive(ShowSecondary);
+        }
     }
 }
