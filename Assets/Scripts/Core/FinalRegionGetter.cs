ea98e25230d8c3b3170dc9c55a4ecea30c56ca73
Francisco Castillo <78894868+frank-castillo@users.noreply.github.com>
2023-05-24 15:56:07 -0700

    [week-8-large-island-ending] Experience Ending implemented

---
+++ Assets/Scripts/Core/FinalRegionGetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalRegionGetter : MonoBehaviour
+{
+    [SerializeField] private Transform _experienceEndingTrigger = null;
+    [SerializeField] private Transform _triggeringNode = null;
+
+    private LevelLoader _levelLoader = null;
+    private bool _experienceEnded = false;
+
+    public void Initialize()
+    {
+        _levelLoader = ServiceLocator.Get<LevelLoader>();
+    }
+
+    private void Update()
+    {
+        if (_triggeringNode.position.z >= _experienceEndingTrigger.position.z && !_experienceEnded)
+        {
+            _experienceEnded = true;
+            _levelLoader.FinalizeExperience();
+        }
+    }
+}
