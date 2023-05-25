16ca681125a3bcb5a992660253447d71d6e8991b
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2023-05-24 17:37:35 -0700

    [Fog Branch] Fog Curve

--- Assets/Scripts/Core/Environment/FogController.cs
+++ Assets/Scripts/Core/Environment/FogController.cs
@@ -3,4 +3,33 @@
 public class FogController : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _closeFog = null;
+    [SerializeField] private AnimationCurve _animationCurve = new AnimationCurve();
+    [SerializeField] private int _changeAmount = 1;
+    private FeedbackManager _feedbackManager = null;
+    private BeatManager _beatManager = null;
+
+    public void Initialize()
+    {
+        _feedbackManager = ServiceLocator.Get<FeedbackManager>();
+        _beatManager = ServiceLocator.Get<BeatManager>();
+
+        Subscriptions();
+    }
+
+    private void Subscriptions()
+    {
+        _feedbackManager.OnBeatHitSubscribe(LowerFog);
+        _feedbackManager.RepeatedMissSubscribe(IncreaseFog);
+    }
+
+    private void LowerFog()
+    {
+        var emission = _closeFog.emission;
+        emission.rateOverTime = 50.0f;
+    }
+
+    private void IncreaseFog()
+    {
+
+    }
 }
