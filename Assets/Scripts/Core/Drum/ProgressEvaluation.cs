141028742c62cdaff1e3df3e989a746aa9d3a071
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2023-06-12 22:41:56 -0700

    [Beat Drip] Progression

---
+++ Assets/Scripts/Core/Drum/ProgressEvaluation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ProgressEvaluation : MonoBehaviour
+{
+    [SerializeField] private float _requiredPercentage = 0.7f;
+    private int _successCounter = 0;
+    private int _totalNotes = 0;
+
+    public void Prepare(int totalNotes)
+    {
+        _totalNotes = totalNotes;
+        _successCounter = 0;
+    }
+
+    public void Success()
+    {
+        ++_successCounter;
+    }
+
+    public void Fail()
+    {
+        if (_successCounter > 0)
+        {
+            --_successCounter;
+        }
+    }
+
+    public bool MoveToNextTier()
+    {
+        float successPercentage = _successCounter / _totalNotes;
+
+        if (successPercentage > _requiredPercentage)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
