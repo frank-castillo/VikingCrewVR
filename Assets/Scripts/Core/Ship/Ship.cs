1e0ec6c69968acb7d10da5752805bd2b9b66fd88
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2023-06-27 20:58:31 -0700

    [Feedback Cleanup] Start Of Cleanup

--- Assets/Scripts/Core/Ship/Ship.cs
+++ Assets/Scripts/Core/Ship/Ship.cs
@@ -9,7 +9,6 @@ public class Ship : MonoBehaviour
     [SerializeField] private List<AnimationFeedback> _vikingsRowing = new List<AnimationFeedback>();
     [SerializeField] private List<Feedback> _rowingSoundFeedback = new List<Feedback>();
     private FeedbackHandler _feedbackHandler = null;
-    private TierToggle _tierToggle = null;
 
     public DrumController Drum { get => _drum; }
 
@@ -18,10 +17,8 @@ public class Ship : MonoBehaviour
         Debug.Log($"<color=Lime> {this.GetType()} starting setup. </color>");
 
         _feedbackHandler = GetComponent<FeedbackHandler>();
-        _tierToggle = GetComponent<TierToggle>();
 
         _feedbackHandler.Initialize();
-        _tierToggle.Initialize();
         _drum.Initialize();
         _sailController.Initialize();
 
